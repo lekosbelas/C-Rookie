a58eaafbd1ff0a4a3db3bb6162ca49ba4f627c2d
Hasan Taskiran <35641898+lekosbelas@users.noreply.github.com>
2023-02-13 16:22:00 +0300

    Add files via upload

---
+++ Find Greatest Multiple of Number/main.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+
+int main() {
+    // get input value for number
+    int number;
+    scanf("%d", &number);
+
+    // run a loop from i = 100 to 1
+    for (int i = 100; i >= 1; i--) {
+        // check if number is divisible by i
+        // if yes, print i and exit loop
+        if(i % number == 0) {
+            printf("%d", i);
+            break;
+        }
+    }
+
+    return 0;
+}
